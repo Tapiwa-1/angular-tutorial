ac09f0af8c344dde6daa77f3d7bd76f8e46572b5
tapiwa-1 <tmotsi10@gmail.com>
2023-09-11 20:44:19 +0200

    add component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 
-@Component({
 
+@Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  template: `<h1>Hello world!</h1>`,
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.png" width="40" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+  
+      <app-home></app-home>
+    </section>
+  </main>
+  `,
 })
+
 export class AppComponent {
-  title = 'homes';
+  title = 'angular-tutorial';
 }
