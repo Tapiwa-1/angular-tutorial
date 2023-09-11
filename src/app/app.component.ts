824660b60ba4b683d128ac9f31ff708a52cfaf56
tapiwa-1 <tmotsi10@gmail.com>
2023-09-11 19:16:13 +0200

    Learn hello world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
+
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  template: `<h1>Hello world!</h1>`,
 })
 export class AppComponent {
-  title = 'angular-tutorial';
+  title = 'homes';
 }
